33171cc48870ffb5cdf9711a5348ba6cff38d2cd
Santiago <santiago20mosqueraperea@gmail.com>
2025-11-06 18:44:24 -0500

    feat: Agregar hero responsive

--- frontend/busca-ofertas/app/routes/home/home.tsx
+++ frontend/busca-ofertas/app/routes/home/home.tsx
@@ -1,7 +1,36 @@
-import Navbar from '../../components/Navbar/Navbar';
+import "./home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import heroImage from "../../assets/hero-image.png";
 
 export default function Home() {
   return (
-    <Navbar/>
+    <>
+      <Navbar />
+
+      {/* Hero Section */}
+      <section className="hero">
+        <div className="container hero-section">
+          <div className="row">
+            <div className="col-12 col-md-6 container-hero-text">
+                <h1>Compra mas inteligente, vende con mayor visibilidad</h1>
+                <h6 className="hero-titles">Para Clientes</h6>
+                <p>
+                  Encuentra las mejores ofertas cerca de ti y ahorra comparando
+                  precios.
+                </p>
+                <h6 className="hero-titles">Para Negocios</h6>
+                <p>
+                  Haz crecer tu negocio publicando productos y ofertas en
+                  minutos.
+                </p>
+            </div>
+
+            <div className="d-none d-md-block col-md-6 container-hero-image">
+              <img src={heroImage} alt="Hero Image" className="hero-image" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
